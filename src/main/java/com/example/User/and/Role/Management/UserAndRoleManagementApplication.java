071d1d15c6375bfeee3bd1ab7cb6a6ea87a8dfb6
Alok Tiwari <alokt7366@gmail.com>
2026-02-06 11:51:27 +0530

    Adding the Entry point

---
+++ src/main/java/com/example/User/and/Role/Management/UserAndRoleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.User.and.Role.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndRoleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAndRoleManagementApplication.class, args);
+	}
+
+}
